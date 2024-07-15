6aac9eaf34caad53c8df51f9b19c5a03c236e1fd
haruka135 <harune135@naver.com>
2024-07-15 21:40:07 +0900

    d

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 const cors = require("cors");
-const port = 8001;
+const port = process.env.PORT || 3000;
 const bodyParser = require("body-parser");
 const mysql = require("mysql");
 
