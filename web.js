6501aaa4b879e43fa1bdd837d46677a7baaa6d76
cotasi135 <cotasi135@gmail.com>
2024-07-16 15:07:49 +0900

    d

--- web.js
+++ web.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 const cors = require("cors");
-const port = process.env.PORT || 8001;
+const port = 8001;
 const mysql = require("mysql2");
 
 const app = express();
